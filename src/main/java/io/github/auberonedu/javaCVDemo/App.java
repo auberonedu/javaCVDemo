9d523374e85f74bc2a542e4a852e5cf06eea6663
Auberon López <auberonedu@gmail.com>
2025-04-27 16:24:15 -0700

    Inital cookie cutter

---
+++ src/main/java/io/github/auberonedu/javaCVDemo/App.java
@@ -0,0 +1,7 @@
+package io.github.auberonedu.javaCVDemo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/io/github/auberonedu/javaCVDemo/AppTest.java
@@ -0,0 +1,11 @@
+package io.github.auberonedu.javaCVDemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testMainRuns() {
+        assertTrue(true);
+    }
+}
